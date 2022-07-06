e287a9f9983d8e588329247d97b7231bc51eb25a
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-07-06 09:31:28 +0530

    only front end by props

---
+++ src/Page/Dashboard/List.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function List({ employees, handleEdit, handleDelete }) {
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: null
+    });
+
+    return (
+        <div className='contain-table'>
+            <table className='striped-table'>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Job Title</th>
+                        <th>Company Name</th>
+                        <th>Company Location</th>
+                        <th>CTC</th>
+                        <th>Interview Stage</th>
+                        <th colSpan={2} className="text-center">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.length > 0 ? (
+                        employees.map((employee, i) => (
+                            <tr key={employee.id}>
+                                <td>{i + 1}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+                                <td>{employee.salary}LPA</td>
+                                <td>{employee.date} </td>
+                                <td className="text-right">
+                                    <button
+                                        onClick={() => handleEdit(employee.id)}
+                                        className="button muted-button"
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                                <td className="text-left">
+                                    <button
+                                        onClick={() => handleDelete(employee.id)}
+                                        className="button muted-button"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={7}>No Jobs</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default List
---
+++ src/data/index.js
@@ -0,0 +1,76 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Full Stack (Mern)',
+    lastName: 'Getsupp',
+    email: 'Gurgaon',
+    salary: '7',
+    date: 'Tech Round'
+  },
+  {
+    id: 2,
+    firstName: 'Frontend (React Js)',
+    lastName: 'Attentive Ai',
+    email: 'Noida',
+    salary: '6',
+    date: 'Online Test'
+  },
+  {
+    id: 3,
+    firstName: 'Backend (Nodejs)',
+    lastName: 'Fisdom',
+    email: 'Bangalore',
+    salary: '9',
+    date: 'Live Interview'
+  },
+  {
+    id: 4,
+    firstName: 'Backend (Nodejs)',
+    lastName: 'Fisdom',
+    email: 'Bangalore',
+    salary: '15',
+    date: 'Screening Call'
+  },
+  {
+    id: 5,
+    firstName: 'Full Stack (Mern)',
+    lastName: 'Expertia',
+    email: 'Bangalore',
+    salary: '12',
+    date: 'Hr Round'
+  },
+  {
+    id: 6,
+    firstName: 'Frontend (React Js)',
+    lastName: 'Amazon',
+    email: 'Gurgaon',
+    salary: '10',
+    date: 'Managerial Round'
+  },
+  {
+    id: 7,
+    firstName: 'Full Stack (Mern)',
+    lastName: 'Propelld',
+    email: 'Odisha',
+    salary: '5',
+    date: 'Online Test'
+  },
+  {
+    id: 8,
+    firstName: 'Backend (Nodejs)',
+    lastName: 'Source ',
+    email: 'Pune',
+    salary: '8',
+    date: '60 Mins Interview'
+  },
+  {
+    id: 9,
+    firstName: 'Full Stack (Mern)',
+    lastName: 'Datamatics',
+    email: 'Surat',
+    salary: '5',
+    date: 'Cto Round'
+  }
+];
+
+export { employeesData };
